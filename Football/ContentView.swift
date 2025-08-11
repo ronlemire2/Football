7296cde6753c2e99aa9abb8e3b102b170d879ea3
ronlemire2 <ronlemire2@gmail.com>
2025-08-11 09:07:12 -0700

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Ron Lemire on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Ron Lemire on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
